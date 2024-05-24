b238465c2eab4efe9f773cdbae9e065e2f2b2482
Hen Simkin <hensi1@ac.sce.ac.il>
2024-05-24 17:12:37 +0300

    changed to hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,HashRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 
 //const publicUrl = "https://profinder-vzbv.onrender.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <SnackbarProvider>
       <App />
     </SnackbarProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
