363fdf93cc623cbcebc33b15629b954350fadc54
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-05-27 10:58:09 +0300

    add npm test

--- test/registerTest.js
+++ test/registerTest.js
@@ -1,18 +1,20 @@
-import { Builder, By, Key, until,Browser } from 'selenium-webdriver';
+import { Builder, By, Key, until, Browser } from 'selenium-webdriver';
 import { assert } from 'chai'; // Import assert function from Chai
 
+describe("register test", function() {
+    it("register test", async function() {
+        // Generate a random number between 0 and 100000
+        const randomNumber = Math.floor(Math.random() * 100001);
+        // Construct the email with the random number
+        const email = `example${randomNumber}@gmail.com`;
 
-describe("register test",function(){
-    it("register test",async function(){
         let driver = await new Builder().forBrowser(Browser.CHROME).build();
         await driver.get('http://localhost:5173/');
         await driver.findElement(By.linkText('Register')).click();
         await driver.findElement(By.name('name')).sendKeys('example');
-        await driver.findElement(By.name('email')).sendKeys('example222@gmail.com');
+        await driver.findElement(By.name('email')).sendKeys(email);
         await driver.findElement(By.name('password')).sendKeys('123456');
         await driver.findElement(By.css('.btn.btn-success')).click();
-        
-
 
         await driver.wait(until.urlIs('http://localhost:5173/LoginPage'), 3000);
 
@@ -24,13 +26,5 @@ describe("register test",function(){
         
         // Close the browser
         await driver.quit();
-
-
     });
 });
-
-
-
-
-
-
